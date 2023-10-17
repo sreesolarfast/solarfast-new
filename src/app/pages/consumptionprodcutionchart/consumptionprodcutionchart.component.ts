f52b511a013008ff1bd492ef53a025b3bcd4d1b8
Masthan9573 <anthatimasthan9515@gmail.com>
2023-10-17 19:39:33 +0530

    map moved to choosepackagecomponent

--- src/app/pages/consumptionprodcutionchart/consumptionprodcutionchart.component.ts
+++ src/app/pages/consumptionprodcutionchart/consumptionprodcutionchart.component.ts
@@ -1,5 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApexOptions, ChartType } from 'ng-apexcharts';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexPlotOptions,
+  ApexStroke,
+  ApexTitleSubtitle,
+  ApexYAxis,
+  ApexTooltip,
+  ApexFill,
+  ApexLegend
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  tooltip: ApexTooltip;
+  fill: ApexFill;
+  legend: ApexLegend;
+};
+
+
+
 
 @Component({
   selector: 'app-consumptionprodcutionchart',
@@ -7,39 +38,70 @@ import { ApexOptions, ChartType } from 'ng-apexcharts';
   styleUrls: ['./consumptionprodcutionchart.component.scss']
 })
 export class ConsumptionprodcutionchartComponent {
-  chartOptions: ApexOptions = {
-    chart: {
-      type: 'bar',
-      height: 500,
-      toolbar: {
-        show: true,
-      },
-    },
-    colors : ['#000', '#fffff'],
-    plotOptions: {
-      bar: {
-        horizontal: true,
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    // xaxis: {
-    //   categories: ['0 kWh', '150 kWh', '300 kWh', '450 kWh', '600 kWh'],
-    // },
-    xaxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    },
-    series: [
-      {
-        name: 'Consumption',
-        data: [50, 75, 80, 60, 90,50, 75, 80, 60, 90,50, 75, 120],
-      },
-      {
-        name: 'Production',
-        data: [30, 40, 50, 60, 70,30, 40, 50, 60, 70,30, 40, 50],
-      },
-    ],
-  };
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
+  constructor() {
+  this.chartOptions ={
+    series: [
+    {
+      name: 'Consumption',
+      group: 'budget',
+      data: [44000, 55000, 41000, 67000, 22000, 43000,44000, 55000, 41000, 67000, 22000, 43000]
+    },
+    {
+      name: 'Prodcution',
+      group: 'actual',
+      data: [48000, 50000, 40000, 65000, 25000, 40000,44000, 55000, 41000, 67000, 22000, 43000]
+    }
+  ],
+    chart: {
+    type: 'bar',
+    height: 350,
+    stacked: false,
+  },
+  stroke: {
+    width: 1,
+    colors: ['#fff']
+  },
+  dataLabels: {
+    // formatter: (val:any) => {
+    //   return val / 1000 + 'KWh'
+    // }
+    enabled: false,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  xaxis: {
+    categories: [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ]
+  },
+  fill: {
+    opacity: 1
+  },
+  // colors: ['#80c7fd', '#008FFB', '#80f1cb', '#00E396'],
+  yaxis: {
+    labels: {
+      formatter: (val) => {
+        return val / 200 + 'KWh'
+      },
+      style: {
+        colors: ["green"],
+        fontSize: '12px',
+        fontFamily: 'Helvetica, Arial, sans-serif',
+        fontWeight: 400,
+        cssClass: 'apexcharts-yaxis-label',
+      },
+    },
+  },
+  legend: {
+    position: 'bottom',
+    horizontalAlign: 'center'
+  }
+  };
+  }
 }
--- src/app/pages/solarfastsystem/solarfastsystem.component.ts
+++ src/app/pages/solarfastsystem/solarfastsystem.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ApexOptions, ChartType } from 'ng-apexcharts';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexPlotOptions,
+  ApexStroke,
+  ApexTitleSubtitle,
+  ApexYAxis,
+  ApexTooltip,
+  ApexFill,
+  ApexLegend
+} from "ng-apexcharts";
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  tooltip: ApexTooltip;
+  fill: ApexFill;
+  legend: ApexLegend;
+};
 @Component({
   selector: 'app-solarfastsystem',
   templateUrl: './solarfastsystem.component.html',
   styleUrls: ['./solarfastsystem.component.scss']
 })
 export class SolarfastsystemComponent {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
+  constructor() {
+  this.chartOptions ={
+    series: [
+    {
+      name: 'Consumption',
+      group: 'cons',
+      data: [44000, 55000, 41000, 67000, 22000, 43000,44000, 55000, 41000, 67000, 22000, 43000]
+    },
+    {
+      name: 'Prodcution',
+      group: 'prod',
+      data: [48000, 50000, 40000, 65000, 25000, 40000,44000, 55000, 41000, 67000, 22000, 43000]
+    }
+  ],
+    chart: {
+    type: 'bar',
+    height: 300,
+    stacked: false,
+  },
+  stroke: {
+    width: 1,
+    colors: ['']
+  },
+  dataLabels: {
+    // formatter: (val:any) => {
+    //   return val / 1000 + 'KWh'
+    // }
+    enabled: false,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  xaxis: {
+    categories: [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ],
+    labels: {
+      style: {
+        colors: 'white',
+      }
+    }
+  },
+  fill: {
+    opacity: 1
+  },
+  yaxis: {
+    labels: {
+      formatter: (val) => {
+        return val / 200 + 'KWh'
+      },
+      style: {
+        colors: ["white"],
+        fontSize: '12px',
+        fontFamily: 'Helvetica, Arial, sans-serif',
+        fontWeight: 400,
+        cssClass: 'apexcharts-yaxis-label',
+      },
+    },
+  },
+  legend: {
+    position: 'bottom',
+    horizontalAlign: 'center',
+    labels: {
+      colors: 'white', // Set your desired color here
+    }
+  },
+  };
+  }
 }
